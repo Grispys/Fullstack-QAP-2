f716dae056a036954dce20d5390a0fdba851cf05
Grispys <grispanimation@gmail.com>
2024-10-09 23:53:06 -0230

    added express sessions as per suggested on stackoverflow, allows function details to be reused

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// i looked up on stackoverflow how one would keep the details from a function call in stasis, so they can be retrieved in another route. This is how i check the questions answer.
+const session = require('express-session')
 const express = require('express');
 const { getQuestion } = require('./utils/mathUtilities');
 const app = express();
@@ -6,7 +8,11 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true })); // For parsing form data
 app.use(express.static('public')); // To serve static files (e.g., CSS)
-
+app.use(session({
+    secret: 'test2',
+    resave: false,
+    saveUninitialized: true
+}));
 
 
 
@@ -21,13 +27,20 @@ app.get('/leaderboards', (req, res) => {
 
 app.get('/quiz', (req, res) => {
     const theQuiz = getQuestion();
+    req.session.theQuiz = theQuiz;
     res.render('quiz',{theQuiz:theQuiz});
 });
 
 //Handles quiz submissions.
 app.post('/quiz', (req, res) => {
     const { answer } = req.body;
-    console.log(`Answer: ${answer}`);
+    const theQuiz = req.session.theQuiz;
+    if(answer != theQuiz.quizAnswer){
+        console.log(`WRONG. CORRECT ANSWER IS ${theQuiz.quizAnswer}`);
+    }else{
+        console.log(`CORRECT. THE ANSWER IS ${answer}`);
+    }
+    
 
     //answer will contain the value the user entered on the quiz page
     //Logic must be added here to check if the answer is correct, then track the streak and redirect properly
